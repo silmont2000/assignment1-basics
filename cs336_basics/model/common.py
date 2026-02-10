a8bc68027c8cd3e3f4832e39ae69a7e4f6568b05
xby <xbyzju@gmail.com>
2026-02-10 22:55:22 +0800

    softmax,attention

---
+++ cs336_basics/model/common.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+from jaxtyping import Float
+from jaxtyping import Bool, Float, Int
+from einops import einsum
+
+
+def softmax(x: Tensor, i: int):
+    max_x = x.max(dim=i, keepdim=True).values
+    numerator = Tensor.exp(x-max_x)
+    denominator = Tensor.sum(numerator, dim=i, keepdim=True)
+    return numerator/denominator
+
+
+def scaled_dot_product_attention(Q: Float[Tensor, " ... queries d_k"],
+                                 K: Float[Tensor, " ... keys d_k"],
+                                 V: Float[Tensor, " ... values d_v"],
+                                 mask: Bool[Tensor,
+                                            " ... queries keys"] | None = None,
+                                 ):
+    scores = einsum(Q, K, "... queries d_k, ... keys d_k -> ... queries keys")
+    scores = scores / (Q.shape[-1] ** 0.5)
+    minus_inf = -1e9  # 当负无穷使
+    if mask is not None:
+        scores = scores.masked_fill(mask == False, minus_inf)
+
+    # 别忘了softmax！！！！
+    weights = softmax(scores, i=-1)
+    return einsum(weights, V, "... queries keys, ... keys d_v -> ... queries d_v")
